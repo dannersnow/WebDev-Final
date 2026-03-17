d5b12ee44ca24ec0d86d7543d9e2c76d615bc20b
dansnow <dansnow@pdx.edu>
2026-03-16 23:52:43 -0700

    Third commit - Fixed small typo in html file. Added simple style file to separate sections and navbar. Added a quick js file for smooth scroll so the navbar does what it is intended to do.

---
+++ script.js
@@ -0,0 +1,13 @@
+// for smooth scrolling, so when i click buttons it moves down to correct element
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    // safety check to avoid crashes
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
